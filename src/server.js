0dfd39f70cc840582674aa2087a52c7778bc58bc
wahyupambudi <wahyupambudi823@gmail.com>
2023-07-13 09:58:23 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require("dotenv").config();
+
 const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
-const NotesService = require("./services/inMemory/NotesService");
 const NotesValidator = require("./validator/notes");
+
+// jika menggunakan memory
+// const NotesService = require("./services/inMemory/NotesService");
+
+// jika menggunakan postgre
+const NotesService = require("./services/postgres/NotesService");
 
 const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ["*"],
---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+// file ini digunakan untuk mapping
+
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+	id,
+	title,
+	body,
+	tags,
+	createdAt: created_at,
+	updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };
